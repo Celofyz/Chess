7d49e9c83e1a6a4b09e9b7006efcfc29a85fc630
Adam Szulak <TheCelofyz@gmail.com>
2017-11-07 18:43:36 +0100

    Update Field.java

--- Chess/src/pl/everfree/celofyz/Field.java
+++ Chess/src/pl/everfree/celofyz/Field.java
@@ -2,9 +2,9 @@ package pl.everfree.celofyz;
 
 /* */
 public class Field {
-	protected ID id;
-	protected int x;
-	protected int y;
+	private ID id;
+	private int x;
+	private int y;
 	
 	public Field(int x, int y, ID id){
 		this.x = x;
